5c95114a65b1b67d643272b9c52e333080886d1b
Dante <dkirsman@gmail.com>
2024-01-01 21:52:03 -0700

    Update alacritty to use new TOML config

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -1,5 +1,6 @@
 require("config.options")
 require("config.keymaps")
+require("config.autocmds")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -160,5 +160,13 @@ return {
         },
       },
     },
+    {
+      "akinsho/toggleterm.nvim",
+      version = "*",
+      keys = {
+        { "<leader>tt", "<cmd>ToggleTerm size=10 direction=horizontal<CR>", desc = "Open a terminal" },
+      },
+      config = true,
+    },
   },
 }
