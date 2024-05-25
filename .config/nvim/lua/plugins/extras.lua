94c6b9933f2b5aad75c4ece3ea383899f0d151b5
Dante <dkirsman@gmail.com>
2024-05-24 23:30:25 -0600

    fix warning

--- .config/nvim/lua/plugins/extras.lua
+++ .config/nvim/lua/plugins/extras.lua
@@ -6,13 +6,14 @@ return {
     "stevearc/dressing.nvim",
     lazy = true,
     init = function()
-      local lazy = require("lazy")
       vim.ui.select = function(...)
-        lazy.load({ plugins = { "dressing.nvim" } })
+        ---@diagnostic disable-next-line: different-requires
+        require("lazy").load({ plugins = { "dressing.nvim" } })
         return vim.ui.select(...)
       end
       vim.ui.input = function(...)
-        lazy.load({ plugins = { "dressing.nvim" } })
+        ---@diagnostic disable-next-line: different-requires
+        require("lazy").load({ plugins = { "dressing.nvim" } })
         return vim.ui.input(...)
       end
     end,
