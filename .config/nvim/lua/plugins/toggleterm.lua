91116e3a421c4227be19445819070b6a379350d9
Dante <dkirsman@gmail.com>
2024-07-27 09:51:08 -0600

    refactor(nvim): going to add it as a submodule on mac setup as well

--- .config/nvim/lua/plugins/toggleterm.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  keys = {
-    { "<leader>tt", "<cmd>ToggleTerm size=10 direction=float<CR>", desc = "Open a terminal" },
-  },
-  config = true,
-}
--- .config/nvim/lua/plugins/trouble.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "folke/trouble.nvim",
-  cmd = { "Trouble" },
-  opts = { use_diagnostic_signs = true },
-  keys = {
-    { "<leader>td", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
-    { "<leader>tD", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
-  },
-}
