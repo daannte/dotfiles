edc68eddcec07468c8cb56b7d2d75bc123585015
Dante <dkirsman@proton.me>
2024-07-30 10:11:40 -0600

    fix(name): change serenity to rose

--- .config/awesome/config/init.lua
+++ .config/awesome/config/init.lua
@@ -4,7 +4,7 @@ Config.terminal = "st"
 Config.browser = "firefox"
 Config.editor = "nvim"
 Config.modkey = "Mod4"
-Config.theme = "serenity"
+Config.theme = "rose"
 
 require("config.client")
 require("config.binds")
