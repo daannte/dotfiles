8be8f3ea4e603316a5e11c4d9157b25001b874c1
Dante <dkirsman@gmail.com>
2025-06-19 22:42:59 -0600

    feat: ✨ Setup git with gpg signing and aliases. (no more lazygit)

--- nix/darwin/home/git.nix
+++ nix/darwin/home/git.nix
@@ -6,18 +6,59 @@
       user = {
         name = "Dante";
         email = "dkirsman@gmail.com";
+        signingKey = "1D380ADC748F68FA";
+      };
+      commit = {
+        gpgSign = true;
+        verbose = true;
+        template = "~/.config/git/template";
+      };
+      status = {
+        branch = true;
+        short = true;
+        showStash = true;
+        showUntrackedFiles = "all";
       };
       init = {
         defaultBranch = "main";
       };
+      diff = {
+        context = 3;
+        renames = "copies";
+        interHunkContext = 10;
+      };
+      pager = {
+        diff = "diff-so-fancy | $PAGER";
+        branch = false;
+        tag = false;
+      };
+      diff-so-fancy = {
+        markEmptyLines = false;
+      };
+      interactive = {
+        diffFilter = "diff-so-fancy --patch";
+        singleKey = true;
+      };
       color = {
         ui = "auto";
       };
+      push = {
+        autoSetupRemote = true;
+        default = "current";
+      };
       pull = {
-        ff = "only";
+        default = "current";
+        rebase = true;
+      };
+      rebase = {
+        autoStash = true;
       };
       fetch = {
         prune = true;
+      };
+      log = {
+        abbrevCommit = true;
+        graphColors = "blue,yellow,cyan,magenta,green,red";
       };
     };
   };
